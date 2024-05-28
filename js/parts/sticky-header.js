646bf85a5ffad85851a071d0922ca0301397c370
yurkaronin <yurkaronin@gmail.com>
2024-05-29 01:45:55 +0300

    Шапка, основное меню и декоративные элементы

--- js/parts/sticky-header.js
+++ js/parts/sticky-header.js
@@ -1,31 +1,31 @@
-// let lastKnownScrollY = 0;
-// let ticking = false;
+let lastKnownScrollY = 0;
+let ticking = false;
 
-// function headerChange() {
-//   let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+function headerChange() {
+  let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
-//   if (scrollTop > 126) {
-//     document.body.classList.add("header-sticky");
-//   } else {
-//     document.body.classList.remove("header-sticky");
-//   }
+  if (scrollTop > 126) {
+    document.body.classList.add("header-sticky");
+  } else {
+    document.body.classList.remove("header-sticky");
+  }
 
-//   ticking = false;
-// }
+  ticking = false;
+}
 
-// function onScroll() {
-//   lastKnownScrollY = window.scrollY;
-//   requestTick();
-// }
+function onScroll() {
+  lastKnownScrollY = window.scrollY;
+  requestTick();
+}
 
-// function requestTick() {
-//   if (!ticking) {
-//     requestAnimationFrame(headerChange);
-//   }
+function requestTick() {
+  if (!ticking) {
+    requestAnimationFrame(headerChange);
+  }
 
-//   ticking = true;
-// }
+  ticking = true;
+}
 
 
-// headerChange();
-// window.addEventListener("scroll", onScroll, { passive: true });
+headerChange();
+window.addEventListener("scroll", onScroll, { passive: true });
