c58f4e48b7fbb520b76365bbcc539c3b19ef6393
yurkaronin <47382042+yurkaronin@users.noreply.github.com>
2024-07-05 02:00:49 +0300

    +

--- js/parts/mobile-menu-and-button.js
+++ js/parts/mobile-menu-and-button.js
@@ -13,3 +13,18 @@ if (buttonMenu) {
 } else {
   console.log('На странице нет кнопки меню');
 };
+
+// var buttonMenu = document.querySelectorAll('.js-menu-btn');
+// if (buttonMenu) {
+//   // var navigation = document.querySelector('.navigation');
+//   // Если navigation тоже существует, назначаем обработчик событий
+//   // if (navigation) {
+//   buttonMenu.addEventListener('click', function () {
+//     buttonMenu.classList.toggle('isActive');
+//     // navigation.classList.toggle('isActive');
+//     document.body.classList.toggle('menu-open');
+//   });
+//   // }
+// } else {
+//   console.log('На странице нет кнопки меню');
+// };
