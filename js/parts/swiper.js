7f1bdbb571d0cf747814031ce1fa430b27df6aa3
RoninsWork <47382042+yurkaronin@users.noreply.github.com>
2024-07-12 09:36:25 +0300

    Адаптив первы четырёх секций на главной странице и слайдер для анонсов

--- js/parts/swiper.js
+++ js/parts/swiper.js
@@ -1,34 +1,31 @@
-document.addEventListener('DOMContentLoaded', function () {
-  if (document.querySelector('.hero-home__slider')) {
-
-    console.log('Слайдер hero-home__slider ЕСТЬ');
+document.addEventListener("DOMContentLoaded", function () {
+  if (document.querySelector(".hero-home__slider")) {
+    console.log("Слайдер hero-home__slider ЕСТЬ");
 
     // const progressCircle = document.querySelector(".hero-home__slider .autoplay-progress svg");
     // const progressContent = document.querySelector(".hero-home__slider .autoplay-progress span");
 
-    const swiper = new Swiper('.hero-home__slider .swiper', {
+    const swiper = new Swiper(".hero-home__slider .swiper", {
       // Optional parameters
       loop: true,
       speed: 600,
       parallax: true,
       autoplay: {
         delay: 25000,
-        disableOnInteraction: true
+        disableOnInteraction: true,
       },
       pagination: {
         el: ".hero-home__slider .swiper-pagination",
-        clickable: true
+        clickable: true,
       },
-
-
     });
   } else {
-    console.log('Слайдера hero-home__slider НЕТ');
-  };
+    console.log("Слайдера hero-home__slider НЕТ");
+  }
 
   // Слайдер с отзывами reviews__slider
-  if(document.querySelector('.reviews__slider')) {
-    console.log('Слайдер reviews__slider ЕСТЬ');
+  if (document.querySelector(".reviews__slider")) {
+    console.log("Слайдер reviews__slider ЕСТЬ");
 
     const swiper = new Swiper(".reviews__slider .swiper", {
       spaceBetween: 30,
@@ -48,13 +45,12 @@ document.addEventListener('DOMContentLoaded', function () {
       },
     });
   } else {
-    console.log('Слайдера reviews__slider НЕТ');
-  };
-
+    console.log("Слайдера reviews__slider НЕТ");
+  }
 
   // Слайдер с логотипами партнёров
-  if(document.querySelector('.partners__slider')) {
-    console.log('Слайдер partners__slider ЕСТЬ');
+  if (document.querySelector(".partners__slider")) {
+    console.log("Слайдер partners__slider ЕСТЬ");
     const swiper = new Swiper(".partners__slider .swiper", {
       slidesPerView: 6,
       spaceBetween: 30,
@@ -72,14 +68,13 @@ document.addEventListener('DOMContentLoaded', function () {
         prevEl: ".partners .swiper-button-prev",
       },
     });
-
   } else {
-    console.log('Слайдера reviews__slider НЕТ');
-  };
+    console.log("Слайдера reviews__slider НЕТ");
+  }
 
   // Слайдер с кастомными отзывами
-  if(document.querySelector('.custom-review__slider')) {
-    console.log('Слайдер custom-review__slider ЕСТЬ');
+  if (document.querySelector(".custom-review__slider")) {
+    console.log("Слайдер custom-review__slider ЕСТЬ");
     const swiper = new Swiper(".custom-review__slider .swiper", {
       slidesPerView: 2,
       spaceBetween: 30,
@@ -97,17 +92,17 @@ document.addEventListener('DOMContentLoaded', function () {
         prevEl: ".custom-review__slider .swiper-button-prev",
       },
     });
-
   } else {
-    console.log('Слайдера custom-review__slider НЕТ');
-  };
+    console.log("Слайдера custom-review__slider НЕТ");
+  }
 
   // Слайдер с фотогалереей на всю ширину
-  if(document.querySelector('.photo-slider')) {
-    console.log('Слайдер photo-slider ЕСТЬ');
+  if (document.querySelector(".photo-slider")) {
+    console.log("Слайдер photo-slider ЕСТЬ");
     const swiper = new Swiper(".photo-slider .swiper", {
-      slidesPerView: 'auto',
+      slidesPerView: "auto",
       spaceBetween: 30,
+      autoHeight: true,
       loop: true,
       freeMode: true,
       autoplay: {
@@ -121,12 +116,45 @@ document.addEventListener('DOMContentLoaded', function () {
       navigation: {
         nextEl: ".photo-slider .swiper-button-next",
         prevEl: ".photo-slider .swiper-button-prev",
-      }
+      },
     });
-
   } else {
-    console.log('Слайдера photo-slider НЕТ');
-  };
+    console.log("Слайдера photo-slider НЕТ");
+  }
 
-
+  // Слайдер с карточками анонса товаров
+  if (document.querySelector(".previews-slider")) {
+    console.log("Слайдер previews-slider ЕСТЬ");
+    let swiper = new Swiper(".previews-slider .swiper", {
+      loop: true,
+      // freeMode: true,
+      autoplay: {
+        delay: 25000,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: ".previews-slider .swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".previews-slider .swiper-button-next",
+        prevEl: ".previews-slider .swiper-button-prev",
+      },
+      // Responsive breakpoints
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        },
+        551: {
+          slidesPerView: 2,
+          spaceBetween: 24,
+        },
+        768: {
+          slidesPerView: 3,
+          spaceBetween: 32,
+        },
+      },
+    });
+  }
 });
